d35b70aa747c2af93aa36b3490610fc2bebdf97f
Stephane Erbrech <stephane.erbrech@gmail.com>
2012-04-24 11:13:58 +0200

    using console colorizer by default

--- firefix.rb
+++ firefix.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
-require './src/console_helpers_platypus.rb'
-#require './src/console_helpers.rb'
+#require './src/console_helpers_platypus.rb'
+require './src/console_helpers.rb'
 require './src/firefix.rb'
 require './config.rb'
 require './src/config.rb'
